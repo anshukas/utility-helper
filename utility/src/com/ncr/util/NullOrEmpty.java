3015b866c8e047d3bad3267f1988ae5258142b70
anshukas <anshubtechinit@gmail.com>
2019-01-26 20:28:21 +0530

    Check null and empty using var argument.

---
+++ utility/src/com/ncr/util/NullOrEmpty.java
@@ -0,0 +1,21 @@
+package com.ncr.util;
+
+public class NullOrEmpty {
+
+	public static void main(String[] args){
+		System.out.println(isNullOrEmpty("a","a","n"));  // false
+		System.out.println(isNullOrEmpty("","",""));  // true
+		System.out.println(isNullOrEmpty("","",null));  // true
+		System.out.println(isNullOrEmpty(null,null,null));  // true
+		System.out.println(isNullOrEmpty(null,null,"a"));  // false
+	}
+	
+	public static boolean isNullOrEmpty(String... strArr) {
+		for (String st : strArr) {
+			if (st != null && !"".equals(st)){
+				return false;
+			}
+		}
+		return true;
+    }
+}
