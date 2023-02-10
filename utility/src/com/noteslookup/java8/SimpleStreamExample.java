40e669bcdac17713d8d37ef832ff4399e528ed63
Anshu Kumar <anshukansakar@gmail.com>
2023-02-10 05:33:49 +0530

    Simple Stream Example - more example.

--- utility/src/com/noteslookup/java8/SimpleStreamExample.java
+++ utility/src/com/noteslookup/java8/SimpleStreamExample.java
@@ -16,5 +16,18 @@ public class SimpleStreamExample {
 		List<Integer> numsList = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
 		List<Integer> oddList =  numsList.stream().filter(i -> i %2 != 0).collect(Collectors.toList());
 		System.out.println("OddList: --> " + oddList);
+		
+		/* To find Even Numbers from a List: */
+		List<Integer> evenList = numsList.stream().filter( i -> i %2 == 0).collect(Collectors.toList());
+		System.out.println("EvenList: --> " +evenList);
+		
+		/* To find Even Numbers from an Array - Boxing - convert primitive values to their wrapper:*/
+		int[] numsArr = {1,2,3,4,5,6,7};
+		List<Integer> evenArrList = Arrays.stream(numsArr).filter(i->i%2 == 0).boxed().collect(Collectors.toList());
+		System.out.println("evenArrList --> " + evenArrList);
+		
+		/* convert from the wrapper class stream to the primitive stream */
+		int sum = Arrays.asList(33,45).stream().mapToInt(i -> i).sum();
+		System.out.println(sum);
 	}
 }
