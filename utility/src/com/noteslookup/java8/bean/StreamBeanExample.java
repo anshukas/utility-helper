530ab210e468ddf1556d978fcc66a7ec1309ca45
Anshu Kumar <anshukansakar@gmail.com>
2023-02-12 22:14:06 +0530

    Java 8 - Stream Object Example

--- utility/src/com/noteslookup/java8/bean/StreamBeanExample.java
+++ utility/src/com/noteslookup/java8/bean/StreamBeanExample.java
@@ -1,8 +1,20 @@
 package com.noteslookup.java8.bean;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamBeanExample {
 
 	public static void main(String[] args) {
 		
+		List<User> userList = Arrays.asList(
+					new User("anshu",33),
+					new User("shiwali", 30),
+					new User("Ram", 20)
+				);
+		/* 1.1 Sorted by age, natural order */
+		List<User> userAgeSorted = userList.stream().sorted((o1,o2) -> o1.getAge() - o2.getAge()).collect(Collectors.toList());
+		System.out.println(userAgeSorted);
 	}
 }
