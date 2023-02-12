393080c87a532f9cfc655d0da7d51267c42c6431
Anshu Kumar <anshukansakar@gmail.com>
2023-02-12 22:02:09 +0530

    Java 8 - Stream Object Example

--- utility/src/com/noteslookup/java8/bean/User.java
+++ utility/src/com/noteslookup/java8/bean/User.java
@@ -2,8 +2,11 @@ package com.noteslookup.java8.bean;
 
 public class User {
 
-	private String fName;
-	private String lName;
-	private int age;
+	private String name; 
+    private int age;   
+    public User(String name, int age) { 
+        this.name = name; 
+        this.age = age; 
+    }
 	
 }
